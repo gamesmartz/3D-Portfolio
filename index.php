4ed506a42fa84ae45dc77578338c844f278fcfdd
gamesmartz <awnold@hotmail.com>
2018-04-01 19:02:36 -0700

    initial setup on index

--- index.php
+++ index.php
@@ -138,54 +138,17 @@
       <a href="https://github.com/gamesmartz"><div style="display: inline-block; padding: 40px;"><img src="assets/img/github-logo.png" alt="GitHub Logo"></div></a>
 
     </div>
-
-      
+          
     </section>
   </div>
-  <footer class="footer">
-    <div class="container text-center">
-      <div class="col-lg-12">
-        <ul class="list-inline">
-          <li class="list-inline-item">
-            <a href="https://www.facebook.com/mnorkaitis">
-              <svg class="svg-inline--fa fa-facebook-square fa-w-14 icon" aria-hidden="true" data-prefix="fab" data-icon="facebook-square"
-                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                <path fill="currentColor" d="M448 80v352c0 26.5-21.5 48-48 48h-85.3V302.8h60.6l8.7-67.6h-69.3V192c0-19.6 5.4-32.9 33.5-32.9H384V98.7c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9H184v67.6h60.9V480H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48z"></path>
-              </svg>
-              <!-- <i class="fab fa-facebook-square icon"></i> -->
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a href="https://www.linkedin.com/in/matthew-norkaitis-8a0a0831">
-              <svg class="svg-inline--fa fa-linkedin-in fa-w-14 icon" aria-hidden="true" data-prefix="fab" data-icon="linkedin-in" role="img"
-                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                <path fill="currentColor" d="M100.3 480H7.4V180.9h92.9V480zM53.8 140.1C24.1 140.1 0 115.5 0 85.8 0 56.1 24.1 32 53.8 32c29.7 0 53.8 24.1 53.8 53.8 0 29.7-24.1 54.3-53.8 54.3zM448 480h-92.7V334.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V480h-92.8V180.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V480z"></path>
-              </svg>
-              <!-- <i class="fab fa-linkedin-in icon"></i> -->
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a href="https://twitter.com/mattnorkaitis">
-              <svg class="svg-inline--fa fa-twitter fa-w-16 icon" aria-hidden="true" data-prefix="fab" data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512" data-fa-i2svg="">
-                <path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-              </svg>
-              <!-- <i class="fab fa-twitter icon"></i> -->
-            </a>
-          </li>
-        </ul>
-      </div>
+  <footer class="footer d-flex justify-content-center align-items-end">
+    <div class="copyright">   
+    © <?php echo date('Y') ?> Matt Norkaitis
     </div>
   </footer>
   </div>
   
   </div>
   </div>
-  <script src="/bundle.js"></script>
-
-
-  <iframe id="fillrChip" class="fillrChip" tabindex="-1" scrolling="no" src="chrome-extension://ojhegjfmbbpahdggoekcbmejnifimeca/chip.html"
-    style="display none; position: absolute; background: transparent; width: 0px; height: 0px; border: none; z-index: 2147483647; box-shadow: 0 0 8px 0 rgba(0,0,0,0.12), 0 8px 8px 0 rgba(0,0,0,0.24);"></iframe>
 </body>
-
 </html>
