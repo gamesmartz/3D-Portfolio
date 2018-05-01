3914cf94d953e6a3b9497062b4fed3e6d51cb661
Matthew A <awnold@hotmail.com>
2018-05-01 15:39:44 -0700

    direct path change

--- modules/navbar-portfolios.php
+++ modules/navbar-portfolios.php
@@ -3,7 +3,7 @@
 <div id="navbar-target" class="container-fluid index-nav navbar">
     <ul class="nav hidden-nav d-flex fixed-top align-items-center justify-content-end nav-bar-shrink">
       <li class="nav-item">
-        <a class="nav-link " href="index">Home</a>
+        <a class="nav-link " href="/">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link active" href="#portfolio">Portfolios</a>
--- modules/navbar.php
+++ modules/navbar.php
@@ -3,7 +3,7 @@
 <div id="navbar-target" class="container-fluid index-nav navbar">
     <ul class="nav hidden-nav d-flex fixed-top align-items-center justify-content-end nav-bar-shrink">
       <li class="nav-item">
-        <a class="nav-link  active" href="index">Home</a>
+        <a class="nav-link  active" href="/">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link " href="#portfolio">Portfolios</a>
