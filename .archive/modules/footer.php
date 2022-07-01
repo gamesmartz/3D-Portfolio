67f47e72ed7f57b34d611125b52db87590b3f266
Matt Norkaitis <awnold@hotmail.com>
2022-07-01 13:17:39 -0700

    php to html and cleanup

---
+++ .archive/modules/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer d-flex justify-content-center align-items-end">
+    <a href="https://www.linkedin.com/in/matthew-norkaitis">
+        <div class="copyright">   
+        © <?php echo date('Y') ?> Matt Norkaitis
+        </div>
+    </a>    
+</footer>
---
+++ .archive/modules/navbar.php
@@ -0,0 +1,20 @@
+<?php //Scroll Spy to Highlight Menu Items based on Page Location - Bootstrap ?>
+<body style="position: relative; " data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<div id="navbar-target" class="container-fluid index-nav navbar">
+    <ul style="    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 1;" class="nav hidden-nav d-flex align-items-center justify-content-end nav-bar-shrink">
+      <li class="nav-item">
+        <a class="nav-link  active" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#portfolio">Portfolios</a>
+      </li>
+      <li style="margin-right: 20px;" class="nav-item far-right-nav-margin">
+        <a class="nav-link " href="#contact">Contact</a>
+      </li>
+    </ul>
+  </div>
